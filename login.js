9aafd9c9056e355e1f66a289ae71a004009b2bbb
MuhammadArsalan <muhammadarsalanforever@gmail.com>
2024-08-17 16:48:51 +0530

    Task Completed

---
+++ login.js
@@ -0,0 +1,33 @@
+
+import {  signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { auth } from "./config.js";
+
+
+const email=document.querySelector('#email')
+const password=document.querySelector('#password');
+const form =document.querySelector('#form');
+
+
+
+form.addEventListener('submit',(event)=>{
+
+event.preventDefault();
+
+signInWithEmailAndPassword(auth, email.value, password.value)
+  .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+      window.location = './index.html';
+  })
+
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+console.log(errorMessage);
+  
+
+});
+
+
+
+})
